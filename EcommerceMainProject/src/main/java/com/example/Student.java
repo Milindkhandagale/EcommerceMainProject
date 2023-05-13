6f70973ffc29f7ea9fddbc3159813f9397073298
Milind <milindkhandagale2018@gmail.com>
2023-05-13 11:23:27 +0530

    run the program

---
+++ EcommerceMainProject/src/main/java/com/example/Student.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Student {
+	
+	private String fName;
+	private String lName;
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("ajay");
+	}
+	
+
+
+}
